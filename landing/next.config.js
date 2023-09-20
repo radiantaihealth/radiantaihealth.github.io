f0d93db0c493f515c83f85976bf8aa0303d9779c
roustemkhamitov <rustem.traveldesk@gmail.com>
2023-09-20 20:02:32 +0400

    Update to allow deploying to app engine

--- landing/next.config.js
+++ landing/next.config.js
@@ -1,8 +1,11 @@
+/** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-const nextConfiguration = {
+
+const nextConfig = {
   target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
+  distDir: 'build',
 };
 
-module.exports = withPlugins([optimizedImages], nextConfiguration);
+module.exports = withPlugins([optimizedImages], nextConfig);
