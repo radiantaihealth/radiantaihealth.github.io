4c22e4411e3c7bf15734915426d6416b20be0b5f
yuvrender-gill <yuvrendergill21@gmail.com>
2023-09-25 14:38:21 -0400

    DEV-0 Compressed images and removing stuff
    Removed unneeded assets and compressed the images

--- landing/src/pages/index.js
+++ landing/src/pages/index.js
@@ -27,7 +27,7 @@ export default function IndexPage() {
           {/* <CallToAction />
           <Featured /> */}
 
-          <Testimonials />
+          {/* <Testimonials /> */}
           {/* <Blogs /> */}
           <FAQ />
           {/* <Subscribe /> */}
